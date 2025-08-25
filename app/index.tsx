7d721bf8c3684fc4591e3a5e2022742445a2c9d6
WOWxf1rst <wowxfrst@gmail.com>
2025-08-25 17:27:42 +0300

    Requests works correctly

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,25 @@
-import { Text, View } from "react-native";
+import { useQuery } from '@tanstack/react-query';
+import { Text, View } from 'react-native';
 
 export default function Index() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['ping'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3000/ping');
+      return await response;
+    },
+  });
+
+  if (isPending) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+  console.log(data);
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
